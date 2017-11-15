351d02408c9653cef8a6a4318e01f7d906a70735
Ashley Oliver <ashley.p.oliver@gmail.com>
2017-11-15 10:14:43 +0000

    Room made an inner class of Roomset. Command Actions made an inner class of Game. Commit prior to the major refactoring of removing CommandAcctions as a distinct class altogether.

--- Version3/Command.cs
+++ Version3/Command.cs
@@ -39,97 +39,80 @@ namespace Version3
     // This class implements commands (simple commands may be declared ad hoc using lambda 
     // notation, see the example in the static constructor of CommandParser
     // which provides a 'default' action)
-    class CommandActions
-    {
-        private Game G;
+    //class CommandActions
+    //{
+    //    private Game G;
 
-        public CommandActions(Game G)
-        {
-            this.G = G;
+    //    public CommandActions(Game G)
+    //    {
+    //        this.G = G;
 
-            //Add all direction variants to commands and point them at 'Move'
-            foreach (string S in DirCanonical.Keys)
-            {
-                CommandParser.AddCommand(S, this.Move);
-            }
-        }
+    //        //Add all direction variants to commands and point them at 'Move'
+    //        foreach (string S in DirCanonical.Keys)
+    //        {
+    //            CommandParser.AddCommand(S, this.Move);
+    //        }
+    //    }
 
-        public void Look(string command = "") =>
-            Console.WriteLine($"You are in {G.PlayerOne.CurrentRoom.Description}\n" +
-                $"{G.PlayerOne.CurrentRoom.GetValidExitsAsString()}");
+    //    public void Look(string command = "") =>
+    //        Console.WriteLine($"You are in {G.PlayerOne.CurrentRoom.Description}\n" +
+    //            $"{G.PlayerOne.CurrentRoom.GetValidExitsAsString()}");
         
 
-        public void Quit(string command="")
-        {
-            Console.WriteLine("The roof falls in on your head. You die.");
-            Thread.Sleep(2000);
-            Environment.Exit(0);
-        }
+    //    public void Quit(string command="")
+    //    {
+    //        Console.WriteLine("The roof falls in on your head. You die.");
+    //        Thread.Sleep(2000);
+    //        Environment.Exit(0);
+    //    }
 
-        public void Move(string command)
-        {
-            command = command.Split(' ')[0];
-            Room cr = G.PlayerOne.CurrentRoom;
-            Dir d = (Dir)Enum.Parse(typeof(Dir), DirCanonical[command], true);                      
-            List<Dir> validExits = cr.GetValidExits();
-            if (!validExits.Contains(d)) { Console.WriteLine("I can't go that way!"); return; }
-            else {
-                Console.WriteLine($"Going {d}");
-                G.PlayerOne.CurrentRoom = cr[d];
+    //    public void Move(string command)
+    //    {
+    //        command = command.Split(' ')[0];
+    //        RoomSet.Room cr = G.PlayerOne.CurrentRoom;
+    //        Dir d = (Dir)Enum.Parse(typeof(Dir), DirCanonical[command], true);                      
+    //        List<Dir> validExits = cr.GetValidExits();
+    //        if (!validExits.Contains(d)) { Console.WriteLine("I can't go that way!"); return; }
+    //        else {
+    //            Console.WriteLine($"Going {d}");
+    //            G.PlayerOne.CurrentRoom = cr[d];
                
-                if (!G.PlayerOne.CurrentRoom.Equals(Exit.Instance))
-                {
-                    G.Turn++;
-                    this.Look();
-                }                                
-                else this.GameOver();
-            }
-        }
+    //            if (!G.PlayerOne.CurrentRoom.Equals(RoomSet.Exit.Instance))
+    //            {
+    //                G.Turn++;
+    //                this.Look();
+    //            }                                
+    //            else this.GameOver();
+    //        }
+    //    }
 
-        public void GameOver(string command = "")
-        {
-            Console.WriteLine($"{G.Turn}: Congratulations {G.PlayerOne.Name}! You have escaped" +
-                $" the Maze of Doom and are assured fame, riches and a free coffee.");
-            Console.WriteLine($"You escaped in {G.Turn} turns. Press any key to quit.");
-            Console.ReadKey();
-            Environment.Exit(0);
-        }
+    //    public void GameOver(string command = "")
+    //    {
+    //        Console.WriteLine($"{G.Turn}: Congratulations {G.PlayerOne.Name}! You have escaped" +
+    //            $" the Maze of Doom and are assured fame, riches and a free coffee.");
+    //        Console.WriteLine($"You escaped in {G.Turn} turns. Press any key to quit.");
+    //        Console.ReadKey();
+    //        Environment.Exit(0);
+    //    }
 
-        // create a dictionary mapping between legitimate string representations of directions
-        // and the actual string used
-        // Accepted at this time [North] [north] [N] [n] => North
-        // Allows multiple forms of a command to be accepted
-        public static readonly Dictionary<string, string> DirCanonical = new Dictionary<string, string>();
-        private static string[] names = Enum.GetNames(typeof(Dir));
-        static CommandActions()
-        {
-            foreach (string name in names)
-            {
-                DirCanonical[name] = name;
-                DirCanonical[name.ToLower()] = name;
-                DirCanonical[name.Substring(0,1).ToLower()] = name;
-                DirCanonical[name.Substring(0, 1).ToUpper()] = name;
-            }
-        }
+    //    // create a dictionary mapping between legitimate string representations of directions
+    //    // and the actual string used
+    //    // Accepted at this time [North] [north] [N] [n] => North
+    //    // Allows multiple forms of a command to be accepted
+    //    public static readonly Dictionary<string, string> DirCanonical = new Dictionary<string, string>();
+    //    private static string[] names = Enum.GetNames(typeof(Dir));
+    //    static CommandActions()
+    //    {
+    //        foreach (string name in names)
+    //        {
+    //            DirCanonical[name] = name;
+    //            DirCanonical[name.ToLower()] = name;
+    //            DirCanonical[name.Substring(0,1).ToLower()] = name;
+    //            DirCanonical[name.Substring(0, 1).ToUpper()] = name;
+    //        }
+    //    }
 
 
-    }
+    //}
 }
 
-/* Mini-spec for the 'generic command described above
- * It is probably best for the direction commands to come last 
- * 
- * Is this a specific command?
- * Is this a direction?
- * I don't recognize the command
- * 
- * This removes the need to 'rethrow' unimplemented directions - they are just unrecognized commands
- * and could drop through
- * Also allows specific commands to override directions - might be useful
- * Implies need to be able to remove commands //TODO ability to remove commands
- * 
- * So, it should check whether the command is in the Dir enum
- * if not bail - execute 'whut' command
- * 
- * If yes use the command as an indexer to update currentRoom
- */
--- Version3/Program.cs
+++ Version3/Program.cs
@@ -5,26 +5,109 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Version3.Dir;
+using System.Threading;
 
 namespace Version3
 {
     class Game
     {
+        public class CommandActions
+        {
+            private Game G;
+
+            public CommandActions(Game G)
+            {
+                this.G = G;
+
+                //Add all direction variants to commands and point them at 'Move'
+                foreach (string S in DirCanonical.Keys)
+                {
+                    CommandParser.AddCommand(S, this.Move);
+                }
+
+                CommandParser.AddCommand("look", Look);
+                CommandParser.AddCommand("quit", Quit);
+                CommandParser.AddCommand("help", (s) => Console.WriteLine("'look' will tell you what you can see\nenter a direction to move that way"));
+
+            }
+
+            public void Look(string command = "") =>
+                Console.WriteLine($"You are in {G.PlayerOne.CurrentRoom.Description}\n" +
+                    $"{G.PlayerOne.CurrentRoom.GetValidExitsAsString()}");
+
+
+            public void Quit(string command = "")
+            {
+                Console.WriteLine("The roof falls in on your head. You die.");
+                Thread.Sleep(2000);
+                Environment.Exit(0);
+            }
+
+            public void Move(string command)
+            {
+                command = command.Split(' ')[0];
+                RoomSet.Room cr = G.PlayerOne.CurrentRoom;
+                Dir d = (Dir)Enum.Parse(typeof(Dir), DirCanonical[command], true);
+                List<Dir> validExits = cr.GetValidExits();
+                if (!validExits.Contains(d)) { Console.WriteLine("I can't go that way!"); return; }
+                else
+                {
+                    Console.WriteLine($"Going {d}");
+                    G.PlayerOne.CurrentRoom = cr[d];
+
+                    if (!G.PlayerOne.CurrentRoom.Equals(RoomSet.Exit.Instance))
+                    {
+                        G.Turn++;
+                        this.Look();
+                    }
+                    else this.GameOver();
+                }
+            }
+
+            public void GameOver(string command = "")
+            {
+                Console.WriteLine($"{G.Turn}: Congratulations {G.PlayerOne.Name}! You have escaped" +
+                    $" the Maze of Doom and are assured fame, riches and a free coffee.");
+                Console.WriteLine($"You escaped in {G.Turn} turns. Press any key to quit.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            // create a dictionary mapping between legitimate string representations of directions
+            // and the actual string used
+            // Accepted at this time [North] [north] [N] [n] => North
+            // Allows multiple forms of a command to be accepted
+            public static readonly Dictionary<string, string> DirCanonical = new Dictionary<string, string>();
+            private static string[] names = Enum.GetNames(typeof(Dir));
+            static CommandActions()
+            {
+                foreach (string name in names)
+                {
+                    DirCanonical[name] = name;
+                    DirCanonical[name.ToLower()] = name;
+                    DirCanonical[name.Substring(0, 1).ToLower()] = name;
+                    DirCanonical[name.Substring(0, 1).ToUpper()] = name;
+                }
+            }
+
+
+        }
+
         public CommandActions CA { get; set; }
         public int Turn { get; set; } = 1;
         public Player PlayerOne { get; private set; }
-        public readonly RoomSet maze = RoomSet.Instance;
-        public Game(Player P) {
+        public readonly RoomSet maze = new RoomSet();
+        public Game(Player P)
+        {
             PlayerOne = P;
             CA = new CommandActions(this);
             this.RoomInitializor();
-            this.CommandInitializor();
         }
 
         static void Main(string[] args)
         {
             Game G = new Game(new Player() { Name = "Ashley" });
-            
+
             while (true)
             {
                 Console.Write($"{G.Turn}: ");
@@ -51,18 +134,8 @@ namespace Version3
             Util.Link(rooms[2], North, rooms[0], South);
 
             //Place exit 
-            rooms[3][South] = Exit.Instance;
+            rooms[3][South] = RoomSet.Exit.Instance;
         }
-
-        // Commands mapping to 'Move' are set up automatically from the Dir Enum
-        // and should not be added here
-        public void CommandInitializor()
-        {
-            CommandParser.AddCommand("look", CA.Look);
-            CommandParser.AddCommand("quit", CA.Quit);
-            CommandParser.AddCommand("help", (s) => Console.WriteLine("'look' will tell you what you can see\nenter a direction to move that way"));
-        }
-
     }
 
     //TODO - Think about this - if the first element were always 'None' then useful exits could be treated as a 1 based array rather than a 0 based array which might lead to cleaner code.
@@ -78,90 +151,97 @@ namespace Version3
             return s.ToString().TrimEnd();
         }
 
-        static public void Link(Room r1, Dir d1, Room r2, Dir d2)
+        static public void Link(RoomSet.Room r1, Dir d1, RoomSet.Room r2, Dir d2)
         {
             Link(r1, d1, r2);
             Link(r2, d2, r1);
-           // r1[d1] = r2; r2[d2] = r1;
         }
 
-        static public void Link(Room r1, Dir d1, Room r2)
+        static public void Link(RoomSet.Room r1, Dir d1, RoomSet.Room r2)
         {
             r1[d1] = r2;
         }
-
-        
     }
 
-    class Room
+    sealed class RoomSet : IEnumerable<RoomSet.Room>
     {
-        private Room[] exits = new Room[Enum.GetNames(typeof(Dir)).Length];
-
-        public string Description { get; set; }
-
-        // An indexer. This is how you make objects indexable, 
-        // ie. how you can use O[n] syntax
-        public Room this[Dir D]
+        public class Room
         {
-            get { return this.exits[(int)D]; }
-            set { this.exits[(int)D] = value; }
-        }
+            private Room[] exits = new Room[Enum.GetNames(typeof(Dir)).Length];
 
-        public Room()
-        {
-            RoomSet.Instance.AddRoom(this);
-        }
+            public string Description { get; set; }
 
-        public Room(string s) : this()
-        {
-            Description = s;
-        }
-
-        public List<Dir> GetValidExits()
-        {
-            List<Dir> VE = new List<Dir>();
-            foreach (Dir D in Enum.GetValues(typeof(Dir)))
-                if (this[D] != null) VE.Add(D);
-            return VE;
-        }
-
-        public string GetValidExitsAsString()
-        {
-            StringBuilder s = new StringBuilder();
-            foreach (Dir d in GetValidExits())
+            // An indexer. This is how you make objects indexable, 
+            // ie. how you can use O[n] syntax
+            // In this case we want to be able to index a room by a direction to get another room 
+            public Room this[Dir D]
             {
-                s.Append($"{d} ");
+                get { return this.exits[(int)D]; }
+                set { this.exits[(int)D] = value; }
             }
-            return $"There are exits {s.ToString()}";
-        }
-    }
 
-    /// <summary>
-    /// Singleton class: the only instance of it is accessible as RoomSet.Instance
-    /// </summary>
-    sealed class RoomSet : IEnumerable<Room>
-    {
-        private static int roomkey = 0;
-        private static readonly RoomSet instance = new RoomSet();
+            public Room(string s)
+            {
+                Description = s;
+            }
+
+            public List<Dir> GetValidExits()
+            {
+                List<Dir> VE = new List<Dir>();
+                foreach (Dir D in Enum.GetValues(typeof(Dir)))
+                    if (this[D] != null) VE.Add(D);
+                return VE;
+            }
+
+            public string GetValidExitsAsString()
+            {
+                StringBuilder s = new StringBuilder();
+                foreach (Dir d in GetValidExits())
+                {
+                    s.Append($"{d} ");
+                }
+                return $"There are exits {s.ToString()}";
+            }
+        }
+
+        public class Exit : Room
+        {
+            private static readonly Exit instance = new Exit("The exit");
+
+            private Exit(string s) : base(s) { }
+
+            public static Exit Instance { get { return instance; } }
+        }
+
+        public RoomSet()
+        {
+            roomset[0] = Exit.Instance;
+        }
+
+        //Important - Exit should be allocated index 0
+        private static int roomkey = 1;
         private Dictionary<int, Room> roomset = new Dictionary<int, Room>();
 
         public int Count { get { return roomset.Count; } }
-  
-        //TODO Add method to find key from description (wrapper around existing Dictionary method)
-        // intent is that the returned id can be stashed for use by an initailaizor that 
-        // wants to create links between rooms
+
+        public int? GetKeyFromDescription(string desc) //returns null if not found, returns first match only
+        {
+            foreach (KeyValuePair<int, Room> KV in roomset)
+                if (desc.Equals(KV.Value.Description)) return KV.Key;
+            return null;
+        }
+
+        //Adding a single room returns the key in case you wish to stash it 
+        //e.g. for linking rooms 
         public int AddRoom(Room R) { roomset[roomkey] = R; return roomkey++; }
 
-        private RoomSet() { }
-
-        public static RoomSet Instance { get { return instance; } }
-
+        // Indexer to allow retrieving rooms by id, thin wrapper on the inner dictionary
         public Room this[int i]
         {
             get { return roomset[i]; }
         }
 
-        //Note Values not Keys ....
+        //Note Values not Keys .... Iterate rooms by descriptions not keys
         public IEnumerator<Room> GetEnumerator()
         {
             return roomset.Values.GetEnumerator();
@@ -172,10 +252,14 @@ namespace Version3
             return this.GetEnumerator();
         }
 
+        //Allow multiple rooms to be added from an array of descriptions
         public void AddMultipleRooms(string[] descs)
         {
             foreach (string s in descs)
-                new Room(s);
+            {
+                AddRoom(new Room(s));
+            }
+
         }
     }
 
@@ -187,19 +271,10 @@ namespace Version3
     /// Created automatically and added to RoomSet like any other Room - this means RoomSet.Instance.Count 
     /// is always at least one.
     /// </remarks>
-    sealed class Exit : Room
-    {
-        private static readonly Exit instance = new Exit() { Description = "The exit" };
-
-        private Exit() { }
-
-        public static Exit Instance { get { return instance; } }
-    }
-
-    //move currentRoom into player - player should know where he is!
+   
     class Player
     {
-        public Room CurrentRoom { get; set; }
+        public RoomSet.Room CurrentRoom { get; set; }
         public string Name { get; set; }
     }
 }
